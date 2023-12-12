24eea8777ae7ed47562993fa832c76d1595056e5
Audreymarleau <marleau.audrey@hotmail.com>
2023-12-12 16:28:02 -0500

    animations et ajouts

---
+++ Assets/SceneTerrain/Scripts/DisappearOnCollision.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearOnCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that triggered the collider is the crowbar.
+        if (other.CompareTag("Crowbar"))
+        {
+            // If the collider belongs to the crowbar, make the planks disappear.
+            gameObject.SetActive(false);
+        }
+    }
+}
