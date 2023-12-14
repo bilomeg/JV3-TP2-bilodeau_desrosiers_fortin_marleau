8e4ffad6f14e402954375612168029fc87d8fd43
EmmyFortin <fortin.emmy@gmail.com>
2023-12-14 14:36:50 -0500

    Revert "réussi lumiere lobyy"
    This reverts commit 3129a2714f48fd5fbb499551a01a9a0d9d979404.

--- Assets/EmmyFortin/ScriptsEmmyFortin/SwitchPanneau.cs
+++ Assets/EmmyFortin/ScriptsEmmyFortin/SwitchPanneau.cs
@@ -17,23 +17,6 @@ public class SwitchPanneau : MonoBehaviour
   private GameObject _switch2Off;
   private bool _toggleSwitch1;
 private bool _toggleSwitch2;
-
-public bool _courantOn;
-   
-
-
-
-
-   [SerializeField]
-  private GameObject _switchOn;
-
-  [SerializeField]
-  private GameObject _switchOff;
-
-[SerializeField]
-  private GameObject _lumiereDuLobby;
-
-  private bool _toggleSwitch;
   public void Switch1OnOff()
   {
     
@@ -44,7 +27,7 @@ public bool _courantOn;
       _switch1Off.SetActive(false);
       _toggleSwitch1 = true;
 
-CourantCheck();
+
   
     }
      else
@@ -64,7 +47,6 @@ CourantCheck();
       _switch2On.SetActive(true);
       _switch2Off.SetActive(false);
       _toggleSwitch2 = true;
-      CourantCheck();
     }
       else
     {
@@ -78,37 +60,6 @@ CourantCheck();
     }
     
     }
-    public void CourantCheck()
-{
-    if (_toggleSwitch1 && _toggleSwitch2)
-        {
-            Debug.Log("all0");
-            _courantOn = true;
-         
-        }
-        else
-        {
-            _courantOn = false;
-       
-        }
-}
-
-   public void AllumerLumiere(){
-    if(_toggleSwitch == false && _courantOn ==true){
-        Debug.Log("yes");
-         _switchOn.SetActive(true);
-      _switchOff.SetActive(false);
-      _toggleSwitch = true;
-      _lumiereDuLobby.SetActive(true);
-      //_sonInterupteur.Play();
-    }
-    else{
-              _switchOn.SetActive(false);
-      _switchOff.SetActive(true);
-      _toggleSwitch = false;
-      _lumiereDuLobby.SetActive(false);
-      //_sonInterupteur.Play();
-    }
-   } 
+    
   
 }
