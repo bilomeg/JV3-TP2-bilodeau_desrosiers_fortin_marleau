98d501fa5434da2adf708e9b95a79b1a609572db
Audreymarleau <marleau.audrey@hotmail.com>
2023-12-14 14:32:53 -0500

    animation, script, audios, grobal volume

--- Assets/SceneTerrain/Scripts/DisappearOnCollision.cs
+++ Assets/SceneTerrain/Scripts/DisappearOnCollision.cs
@@ -5,12 +5,46 @@ using UnityEngine;
 public class DisappearOnCollision : MonoBehaviour
 {
     [SerializeField] private GameObject crowbarObject;
+    [SerializeField] private AudioClip disappearanceSound;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        // Get or add an AudioSource component to the GameObject.
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        // Set the audio clip for the disappearance sound.
+        audioSource.clip = disappearanceSound;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == crowbarObject)
         {
-            gameObject.SetActive(false);
+            // Play the disappearance sound using PlayOneShot.
+            if (disappearanceSound != null)
+            {
+                audioSource.PlayOneShot(disappearanceSound);
+                
+                // Delay the destruction of the GameObject to allow the sound to play.
+                Invoke("DestroyGameObject", disappearanceSound.length);
+            }
+            else
+            {
+                // If no sound is available, destroy the GameObject immediately.
+                DestroyGameObject();
+            }
         }
+    }
+
+    // Method to destroy the GameObject.
+    private void DestroyGameObject()
+    {
+        Destroy(gameObject);
     }
 }
